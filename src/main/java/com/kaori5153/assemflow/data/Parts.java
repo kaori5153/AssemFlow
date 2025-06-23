a81ede39ad15ec6682df42db452c92ac94be39c9
kaori5153 <tp17.rnksk@icloud.com>
2025-06-24 00:06:54 +0900

    ER図の修正及びクラス設計の記述を追加

--- src/main/java/com/kaori5153/assemflow/data/Parts.java
+++ src/main/java/com/kaori5153/assemflow/data/Parts.java
@@ -1,5 +1,40 @@
 package com.kaori5153.assemflow.data;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import java.time.LocalDate;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Schema(description = "部品")
+@Getter
+@Setter
+@AllArgsConstructor
+@Builder
 public class Parts {
+
+  @Schema(description = "部品ID", type = "int", example = "1", required = true)
+  @NotNull
+  @Max(999)
+  private int partId;
+
+  @Schema(description = "部品名", type = "String", example = "ねじ", required = true)
+  @NotNull
+  @Size(min = 1, max = 100)
+  private String partName;
+
+  @Schema(description = "製造会社", type = "String", example = "ねじ会社", required = true)
+  @Size(min = 1, max = 100)
+  private String manufacturer;
+
+  @Schema(description = "EOL期日", type = "LocalDate", example = "2023-12-31")
+  private LocalDate eol;
+
+  @Schema(description = "終売フラグ", type = "boolean")
+  private boolean discontinued;
 
 }
--- src/main/java/com/kaori5153/assemflow/data/RequiredParts.java
+++ src/main/java/com/kaori5153/assemflow/data/RequiredParts.java
@@ -1,5 +1,38 @@
 package com.kaori5153.assemflow.data;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Schema(description = "組み立てに必要な部品")
+@Getter
+@Setter
+@AllArgsConstructor
+@Builder
 public class RequiredParts {
+
+  @Schema(description = "必要部品ID", type = "int", example = "1", required = true)
+  @NotNull
+  @Max(999)
+  private int requiredPartId;
+
+  @Schema(description = "組み立て手順ID", type = "int", example = "1", required = true)
+  @NotNull
+  @Max(999)
+  private int procedureId;
+
+  @Schema(description = "部品ID", type = "int", example = "1", required = true)
+  @NotNull
+  @Max(999)
+  private int partId;
+
+  @Schema(description = "必要数", type = "int", example = "1", required = true)
+  @NotNull
+  @Max(999)
+  private int quantity;
 
 }
